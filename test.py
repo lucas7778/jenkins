36b5af2cc0a55a9cdc3be163548004ed2ef3607b
lucas7778 <67800019+lucas7778@users.noreply.github.com>
2022-03-16 15:32:40 -0300

    Update test.py

--- test.py
+++ test.py
@@ -7,7 +7,7 @@ class Test(unittest.TestCase):
     def test(self):
         a = MMQ([1, 2, 3], [10, 20, 30])
         b = a.mmq()
-        self.assertEqual()
+        self.assertEqual(b,0)
 
 
 if __name__ == "__main__":
