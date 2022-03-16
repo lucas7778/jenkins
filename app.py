d6de0c5527bf8b37a2f254574a6961a55fa5bce1
lucas7778 <67800019+lucas7778@users.noreply.github.com>
2022-03-16 14:48:16 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,35 @@
-class Sum():
-    def __init__(self,x,y):
-        self.x==x
-        self.y == y
+from numpy import array
+from numpy.linalg import solve
 
-    @property
+
+class MMQ:
+    def __init__(self, x, y):
+        self.__x = x
+        self.__y = y
+
+
     def x(self):
-        return self.x
-    @x.setter
-    def y(self):
-        return self.y
-    y.setter
+        return self.__x
 
-    def sum(self):
-        return self.x +self.y
+    def y(self):
+        return self.__y
+
+    def mmq(self):
+        sx2 = []
+        tx = len(self.__x)
+        sx = sum(self.__x)
+        for i in range(0, len(self.__x)):
+            x2 = self.__x[i] ** 2
+            sx2.append(x2)
+            s = sum(sx2)
+        sy = sum(self.__y)
+        spxy = []
+        for i in range(0, len(self.__x)):
+            p = self.__x[i] * self.__y[i]
+            spxy.append(p)
+            sxy = sum(spxy)
+
+        x_ = array([[tx, sx], [sx, s]])
+        y_ = array([sy, sxy])
+        c = solve(x_, y_)
+        return print(c[0])
